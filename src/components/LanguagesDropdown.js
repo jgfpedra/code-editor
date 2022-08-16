c15da6d82e0fb551bc5435b3f8cb7832e3d4f74f
jgfpedra <joaogfp07@gmail.com>
2022-08-16 13:54:07 -0300

    code-editor base

---
+++ src/components/LanguagesDropdown.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Select from "react-select";
+import { customStyles } from "../constants/customStyles";
+import { languageOptions } from "../constants/languageOptions";
+
+const LanguagesDropdown = ({ onSelectChange }) => {
+  return (
+    <Select
+      placeholder={`Filter By Category`}
+      options={languageOptions}
+      styles={customStyles}
+      defaultValue={languageOptions[0]}
+      onChange={(selectedOption) => onSelectChange(selectedOption)}
+    />
+  );
+};
+export default LanguagesDropdown;
